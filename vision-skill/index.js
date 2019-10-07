f43fd173a89321c7fd9215fe593f75a4924dcef9
Nathan Gaskill <nathang@ANSGRP-002830.local>
2019-10-07 21:54:38 +0100

    Created new skill

---
+++ vision-skill/index.js
@@ -0,0 +1,81 @@
+module.exports = async function (context, req) {
+    context.log("Image Processing Function Started!!!!");
+
+    //Import Modules
+    var Vision = require('azure-cognitiveservices-vision');
+    var CognitiveServicesCredentials = require('ms-rest-azure').CognitiveServicesCredentials;
+
+    //Cognitive Services API Credentials
+    var serviceKey = '71c926b6c944490c9a3cd3da3cf1083b';
+    var region = 'westeurope';
+    var credentials = new CognitiveServicesCredentials(serviceKey);
+    var computerVisionApiClient = new Vision.ComputerVisionAPIClient(credentials, region);
+
+    //Create empty Array for output
+    var values = [];
+
+    //Parse all records input
+    for(var value of req.body.values){
+
+        //Input Blob
+        var myBlob = value.data.imageData;
+
+        //Call Image Service
+        var text = await imageQuery(myBlob);
+
+        var record = {
+            "recordId": value.recordId,
+            "data": {
+                "descriptions": [
+                    {
+                        "value": "description",
+                        "description": text
+                    }
+                ]
+            },
+            "errors": [],
+            "warnings": []
+        }
+
+        values.push(record);
+
+    };
+
+
+    //Response Body
+    var body = {
+        "values": [values]
+    };
+
+    //Log Response Body
+    context.log(JSON.stringify(body));
+
+    //Return Response
+    context.res = {
+        status: 200,
+        body: body
+    };
+
+    
+    //Function to get Image Attributes
+    async function imageQuery(myBlob){
+        context.log("Calling Vision API");
+
+        await computerVisionApiClient.analyzeImageInStream(myBlob, {visualFeatures: ["Categories", "Tags", "Description", "Color"]})
+          
+            .then(async function(data){    
+                // Log response
+                context.log("data: " + JSON.stringify(data));
+
+                return data.description.captions[0].text
+
+            })
+
+            .catch(function(err) {
+                context.log("Error: " + err);
+                context.done(null, err);
+            })
+
+    };  
+
+};
