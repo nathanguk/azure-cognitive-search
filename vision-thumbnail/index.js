2ef611df3d28357d8588201de16bf7e62306a135
Nathan Gaskill <nathang@ANSGRP-002830.local>
2019-10-10 22:34:49 +0100

    updated  echo function

--- vision-thumbnail/index.js
+++ vision-thumbnail/index.js
@@ -2,13 +2,6 @@ module.exports = async function (context, req) {
     context.log("Image Processing Function Started!!!!");
     context.log(JSON.stringify(context));
 
-    //Import Modules
-    var request = require("request-promise");
-
-    //Cognitive Services API Credentials
-    var serviceKey = process.env.VISION_KEY;
-    var region = process.env.VISION_REGION;
-
     //Create empty Array for output
     var values = [];
 
@@ -16,10 +9,23 @@ module.exports = async function (context, req) {
     for(var value of req.body.values){
 
         //Input Blob
-        let buff = Buffer.from(value.data.imageData.data, 'base64'); 
+        var b64image = value.data.imageData.data
 
-        //Call Image Service and get record
-        await thumbnail(buff, value);
+        var record = {
+            "recordId": value.recordId,
+            "data": {
+                "thumbnail": [
+                    {
+                        "value": "thumbnail",
+                        "thumbnail": b64image
+                    }
+                ]
+            },
+            "errors": null,
+            "warnings": null
+        }
+
+        values.push(record);
 
     };
 
@@ -37,65 +43,5 @@ module.exports = async function (context, req) {
         status: 200,
         body:body
     };
-
-    //Function to create Thumbnail
-    async function thumbnail(image, value) {
-
-        context.log("Calling Thumbnail API");
-
-        var options = { 
-            method: 'POST',
-            url: 'https://'+region+'.api.cognitive.microsoft.com/vision/v1.0/generateThumbnail',
-            qs: { width: '95', height: '95', smartCropping: 'true' },
-            headers: { 
-                'Cache-Control': 'no-cache',
-                'Ocp-Apim-Subscription-Key': serviceKey,
-                'Content-Type': 'application/octet-stream' 
-            },
-            body: image,
-            encoding: null,
-            json: false
-        };
-
-        await request(options)
-            .then(function (body) {
-                //Write to Blob storage
-                context.bindings.outputBlob = body;
-
-                var record = {
-                    "recordId": value.recordId,
-                    "data": {
-                        "thumbnail": [
-                            {
-                                "value": "thumbnail",
-                                "thumbnail": "dummyurl"
-                            }
-                        ]
-                    },
-                    "errors": null,
-                    "warnings": null
-                }
-        
-                values.push(record);
-
-            })
-            .catch(function (err) {
-                context.log('Caught Error: ' + err.message);
-
-                var record = {
-                    "recordId": value.recordId,
-                    "data": null,
-                    "errors": null,
-                    "warnings": [
-                        {
-                            "message": err.message
-                        }
-                    ]
-                }
-        
-                values.push(record);
-            });
-
-        };
 
 };
