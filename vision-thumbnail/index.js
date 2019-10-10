6b9dcf685e2d7eb823c2705c8e4fcffa411b7eb0
Nathan Gaskill <nathang@ANSGRP-002830.local>
2019-10-10 19:09:40 +0100

    additonal modules

---
+++ vision-thumbnail/index.js
@@ -0,0 +1,100 @@
+module.exports = async function (context, req) {
+    context.log("Image Processing Function Started!!!!");
+
+    //Import Modules
+    var request = require("request-promise");
+
+    //Cognitive Services API Credentials
+    var serviceKey = process.env.VISION_KEY;
+    var region = process.env.VISION_REGION;
+
+    //Create empty Array for output
+    var values = [];
+
+    //Parse all records input
+    for(var value of req.body.values){
+
+        //Input Blob
+        let buff = Buffer.from(value.data.imageData.data, 'base64'); 
+
+        //Call Image Service and get record
+        await thumbnail(buff, value);
+
+    };
+
+
+    //Response Body
+    var body = {
+       "values": values
+    };
+
+    //Return Response
+    context.res = {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        status: 200,
+        body:body
+    };
+
+    //Function to create Thumbnail
+    async function thumbnail(image, value) {
+
+        context.log("Calling Thumbnail API");
+
+        var options = { 
+            method: 'POST',
+            url: 'https://'+region+'.api.cognitive.microsoft.com/vision/v1.0/generateThumbnail',
+            qs: { width: '95', height: '95', smartCropping: 'true' },
+            headers: { 
+                'Cache-Control': 'no-cache',
+                'Ocp-Apim-Subscription-Key': serviceKey,
+                'Content-Type': 'application/octet-stream' 
+            },
+            body: image,
+            encoding: null,
+            json: true
+        };
+
+        await request(options)
+            .then(function (body) {
+                //Write to Blob storage
+                context.bindings.outputBlob = body;
+
+                var record = {
+                    "recordId": value.recordId,
+                    "data": {
+                        "descriptions": [
+                            {
+                                "value": "thumbnail",
+                                "thumbnail": "dummyurl"
+                            }
+                        ]
+                    },
+                    "errors": null,
+                    "warnings": null
+                }
+        
+                values.push(record);
+
+            })
+            .catch(function (err) {
+                context.log('Caught Error: ' + err.message);
+
+                var record = {
+                    "recordId": value.recordId,
+                    "data": null,
+                    "errors": null,
+                    "warnings": [
+                        {
+                            "message": err.message
+                        }
+                    ]
+                }
+        
+                values.push(record);
+            });
+
+        };
+
+};
